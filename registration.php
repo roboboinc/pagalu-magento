8b5bdcedfbb8b221b1a1bfa9e7ccd5a5a5b5987a
Vasco Xavier <vliscoxavier06@gmail.com>
2019-11-15 14:42:51 +0200

    override payment method gateway sample

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PagaLu_Payment',
+    'Magento_SamplePaymentGateway',
     __DIR__
 );
