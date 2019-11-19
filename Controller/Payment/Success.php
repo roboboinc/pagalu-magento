8ce478ba1e5fb6cf149517e8c018352f9636cb0e
Vasco Xavier <vliscoxavier06@gmail.com>
2019-11-19 18:53:51 +0200

    temp soluction to update transaction status

--- Controller/Payment/Success.php
+++ Controller/Payment/Success.php
@@ -13,7 +13,7 @@ class Success extends \Magento\Framework\App\Action\Action
         \Magento\Framework\App\Action\Context $context,
         \Magento\Sales\Model\Service\InvoiceService $_invoiceService,
         \Magento\Sales\Model\Order $_order,
-        \Magento\Framework\DB\Transaction $_transaction
+        \Magento\Framework\DB\Transaction $_transaction,
     ) {
         $this->_invoiceService = $_invoiceService;
         $this->_transaction    = $_transaction;
@@ -27,18 +27,18 @@ class Success extends \Magento\Framework\App\Action\Action
 
         try {
             // get post data
-            $postData = $this->getRequest()->getPostValue();
+            $postData = $request->getRequest()->getParams();
 
             // if data looks fine
-            if (isset($postData['orderid']) && isset($postData['paymentRef'])) {
+            // if (isset($postData['orderid']) && isset($postData['paymentRef'])) {
 
                 // get object manager
                 $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 
                 // set order status
-                $this->_order->loadByIncrementId($postData['orderid']);
+                $this->_order->loadByIncrementId(settype($postData['orderid'], 'int'));
                 $this->_order->setState($this->_order->getState())->save();
-                $this->_order->setStatus('payment_review')->save();
+                $this->_order->setStatus('complete')->save();
                 $this->_order->addStatusToHistory($this->_order->getStatus(), __('Payment successful. Transaction ID: ') . $postData['paymentRef'])->save();
                 $this->_order->save();
 
@@ -48,9 +48,9 @@ class Success extends \Magento\Framework\App\Action\Action
 
                 // redirect to success page
                 $this->_redirect('checkout/onepage/success');
-            } else {
-                $this->_redirect('/');
-            }
+            // } else {
+            //     $this->_redirect('/');
+            // }
         } catch (Exception $e) {
             echo $e;
         }
