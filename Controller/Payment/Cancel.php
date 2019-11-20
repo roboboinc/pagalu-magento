12586a2b617c7b833c908df53bde7bba0291674d
Vasco Xavier <vliscoxavier06@gmail.com>
2019-11-20 15:56:03 +0200

    update transaction status to canceled

--- Controller/Payment/Cancel.php
+++ Controller/Payment/Cancel.php
@@ -5,46 +5,48 @@ namespace Magento\PagaLuPaymentGateway\Controller\Payment;
 class Cancel extends \Magento\Framework\App\Action\Action
 {
     public $context;
+    protected $_invoiceService;
     protected $_order;
-    protected $_onepage;
+    protected $transaction;
+    protected $request;
+    protected $transactionRepository;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Sales\Model\Order $_order,
-        \Magento\Checkout\Model\Type\Onepage $_onepage
+        \Magento\Sales\Model\Service\InvoiceService $_invoiceService,
+        \Magento\Sales\Model\Order $order,
+        \Magento\Framework\App\Request\Http $request,
+        \Magento\Framework\DB\Transaction $transaction,
+        \Magento\Sales\Api\TransactionRepositoryInterface $transactionRepository
     ) {
-        $this->context   = $context;
-        $this->_order    = $_order;
-        $this->_onepage = $_onepage;
+        $this->_invoiceService = $_invoiceService;
+        $this->transaction    = $transaction;
+        $this->_order          = $order;
+        $this->context         = $context;
+        $this->request = $request;
+        $this->transactionRepository = $transactionRepository;
         parent::__construct($context);
     }
 
     public function execute()
     {
-
         try {
-            $postData = $this->getRequest()->getPostValue();
+            // parse GET data
+            $order_id = $this->request->getParams();
+            $order_id = (int) $order_id['order_id'];
 
-            if (isset($postData['orderid'])) {
-                // get object manager
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-
-                // load order
-                $this->_order->loadByIncrementId($postData['orderid']);
-
-                // add information
-                $this->_order->addStatusToHistory($this->_order->getStatus(), __('Payment successful. Transaction ID: ').$postData['paymentRef'])->save();
-
-                // update order
+            if (isset($order_id)) {
+                $this->_order->loadByIncrementId($order_id);
+                $this->_order->setState($this->_order->getState())->save();
+                $this->_order->setStatus('canceled')->save();
+                $this->_order->addStatusToHistory($this->_order->getStatus(), __('Payment successful. Transaction ID: '))->save();
                 $this->_order->save();
-
-                // redirect to failure
                 $this->_redirect('checkout/onepage/failure');
             } else {
                 $this->_redirect('/');
             }
         } catch (Exception $e) {
-            echo $e;
+        	echo $e;
         }
     }
 }
