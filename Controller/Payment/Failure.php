0534b23570053940f8fb7fe0525be8b0871d779d
Vasco Xavier <vliscoxavier06@gmail.com>
2019-11-22 13:43:16 +0200

    update faiure view

--- Controller/Payment/Failure.php
+++ Controller/Payment/Failure.php
@@ -2,56 +2,94 @@
 
 namespace Magento\PagaLuPaymentGateway\Controller\Payment;
 
-class Failure extends \Magento\Framework\App\Action\Action
+class Cancel extends \Magento\Framework\App\Action\Action
 {
+
     public $context;
+    protected $_helper;
+    protected $_invoiceService;
     protected $_order;
+    protected $request;
+    protected $transaction;
+    protected $transactionRepository;
+
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Sales\Model\Order $_order,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Sales\Model\Service\InvoiceService $_invoiceService,
+        \Magento\Sales\Model\Order $order,
+        \Magento\Framework\App\Request\Http $request,
+        \Magento\Framework\DB\Transaction $transaction,
+        \Magento\Sales\Api\TransactionRepositoryInterface $transactionRepository,
+        \Magento\PagaLuPaymentGateway\Helper\Data $_helper
     ) {
-        $this->context = $context;
-        $this->_order = $_order;
-        $this->resultPageFactory = $resultPageFactory;
+        $this->_helper         = $_helper;
+        $this->_invoiceService = $_invoiceService;
+        $this->_order          = $order;
+        $this->context         = $context;
+        $this->request         = $request;
+        $this->transaction     = $transaction;
+        $this->transactionRepository = $transactionRepository;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        // TEMP Redirect
-        $this->_redirect('/');
-
         try {
-            $postData = $this->getRequest()->getPostValue();
+            // parse GET data
+            $parameters = $this->request->getParams();
+            $payment_uuid = $parameters['payment_uuid'];
 
-            if (isset($postData['orderid']) && isset($postData['paymentRef'])) {
-                // get object manager
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-
-                // load order
-                $this->_order->loadByIncrementId($postData['orderid']);
-
-                $this->_order->addStatusToHistory($this->_order->getStatus(),__('Payment NOT successful. Transaction ID: ') . $postData['paymentRef'])->save();
-
-                // update order
-                $this->_order->save();
-
-            } else {
+            if (isset($payment_uuid)) {
+                $this->getTransactionStatusOnPagalu($payment_uuid);
+            }else {
                 $this->_redirect('/');
             }
-
-            $resultPage = $this->resultPageFactory->create();
-
-            $block = $resultPage->getLayout()
-                    ->createBlock('Magento\PagaLuPaymentGateway\Block\Payment\Redirect')
-                    ->setTemplate('PagaLuPaymentGateway::payment/failure.phtml')
-                    ->toHtml();
-            $this->getResponse()->setBody($block);
-            return $this->resultPageFactory->create();
         } catch (Exception $e) {
-            echo $e;
+        	echo $e;
         }
+    }
+
+    public function updateTransactionOnMagento($order_id=1){
+        $orderId = (int)$order_id;
+
+        $this->_order->loadByIncrementId($orderId);
+        $this->_order->setState($this->_order->getState())->save();
+        $this->_order->setStatus('canceled')->save();
+        $this->_order->addStatusToHistory($this->_order->getStatus(), __('Payment successful. Transaction ID: '))->save();
+        $this->_order->save();
+        $this->_redirect('checkout/onepage/failure');
+    }
+
+    public function getTransactionStatusOnPagalu($payment_uuid='') {
+        $ch = curl_init();
+        $authorization = "Authorization: Bearer ".$this->_helper->pagalu_api_key();
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_POSTREDIR, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, 'http://sandbox.pagalu.co.mz/pagamento-ext/api/pay-ext/'.$payment_uuid);
+
+        $server_output = curl_exec ($ch);
+        //close connection
+        curl_close ($ch);
+        flush();
+
+        $json = json_decode($server_output, true);
+
+        if (json_last_error() == JSON_ERROR_NONE) {
+            if(isset($json['status'])){
+                $order_id = (int)$json['reference'];
+                $this->updateTransactionOnMagento($order_id);
+            }else {
+                echo "Nao tem status: ".$json;
+            }
+
+        }else {
+            echo "tem erros graves... ".$json;
+        }
+
     }
 }
